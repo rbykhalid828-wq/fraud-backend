e5d74332e6f30edde3d58f720e8a180049460161
rbykhalid828-wq <rbykhalid828@gmail.com>
2026-03-28 23:28:01 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pickle
+
+app = Flask(__name__)
+CORS(app)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return "Fraud Detection API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    features = data.get("features", [0]*30)
+
+    result = model.predict([features])
+
+    return jsonify({
+        "prediction": int(result[0]),
+        "message": "Fraud Detected" if result[0] == 1 else "Not Fraud"
+    })
+
+if __name__ == "__main__":
+    app.run()
