ec95fc4b83c1e2b7949f22675a1482c52b7f2c1b
rbykhalid828-wq <rbykhalid828@gmail.com>
2026-03-29 02:21:07 +0300

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/predict": {"origins": "*"}})
 
 model = pickle.load(open("model.pkl", "rb"))
 
