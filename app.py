7efbb7797ecbdf6aa64c5f9124aa9a3ed035b6f7
rbykhalid828-wq <rbykhalid828@gmail.com>
2026-03-29 01:51:17 +0300

    app.py

--- app.py
+++ app.py
@@ -9,22 +9,13 @@ model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return "Fraud Detection API Running"
+    return "Backend is running"
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    features = data.get("features", [0]*30)
+    features = data["features"]
 
     result = model.predict([features])
 
-    return jsonify({
-        "prediction": int(result[0]),
-        "message": "Fraud Detected" if result[0] == 1 else "Not Fraud"
-    })
-
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    return jsonify({"result": str(result[0])})
